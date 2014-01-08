a35a3911401c2f16a032359ef548c40d11457244
Rasmus Rygaard <rasmus@rasmusrygaard.com>
2014-01-08 15:49:44 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fitbit::Application.config.secret_key_base = '236f9dca2089d4e9c709c77d0cc40367b02bd7172c81a85e49503aa093e15480530da48d660dc189f53df29766632884c5a3bb212738ad2a2e057cf95df8e45b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fitbit::Application.config.session_store :cookie_store, key: '_fitbit_session'
