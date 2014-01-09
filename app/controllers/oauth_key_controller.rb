094c649619d29623f751ddf0c0cf4b8a0ab5faaa
Rasmus Rygaard <rasmus@rasmusrygaard.com>
2014-01-08 20:18:35 -0800

    Debugging key.

--- app/controllers/oauth_key_controller.rb
+++ app/controllers/oauth_key_controller.rb
@@ -2,12 +2,14 @@ class OauthKeyController < ApplicationController
 
   def create
     auth = request.env["omniauth.auth"]
+    puts auth
     key = OathKey.create({
       provider: auth.provider,
       uid: auth.uid,
       token: auth.oauth_token,
       token_secret: auth.oauth_token_secret
     })
+    puts key
     key.save!
     render json: 'ok!', status: 200
   end
