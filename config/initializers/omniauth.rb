25251cce0c4e6f9c9d04559940e6733f66818cbe
Rasmus Rygaard <rasmus@rasmusrygaard.com>
2014-01-15 11:52:40 -0800

    Updated quanto credentials.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,4 @@
-# require 'omniauth-quanto'
-
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :fitbit, 'c42a6a7099f54e0ab9146fa355604a14', '01371805cee6429da70c5dab1f3d1705'
-  provider :quanto, '90e41283b1c888e8993bf75cea806caef6ae7bf9fb67b9ebed39eff8e4ab6187', 'f9900c9cbb2b26e384a4998d76bdfef6053554d59127a9993ddb86aa49a6eec0'
+  provider :quanto, 'd9e1c99976f8a1587f5a6815f6f0581a4ca8fd1fc4e432f43d9c08640eafe9ad', '7d53ec7c6ed77cd91b4cbd16565903d562737fcdec1e515e39c60eecd9fda13c'
 end
-
-puts OmniAuth::Strategies.constants
