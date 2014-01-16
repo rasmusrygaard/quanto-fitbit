769e681a44a7bb08888e370c2e04ad2c57f20373
Rasmus Rygaard <rasmus@rasmusrygaard.com>
2014-01-15 16:34:43 -0800

    More prints.

--- app/controllers/oauth_key_controller.rb
+++ app/controllers/oauth_key_controller.rb
@@ -2,7 +2,9 @@ class OauthKeyController < ApplicationController
 
   def create
     auth = request.env["omniauth.auth"]
+    puts '-------------------------------------------------------'
     puts auth.inspect
+    puts '-------------------------------------------------------'
     key = OauthKey.create({
       provider: auth.provider,
       uid: auth.uid,
