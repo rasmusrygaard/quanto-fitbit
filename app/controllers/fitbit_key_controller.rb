b222aa73e6938b9dafdde253cce689fac3b0de09
Rasmus Rygaard <rasmus@rasmusrygaard.com>
2014-01-26 21:31:23 -0800

    Fixed provider.

--- app/controllers/fitbit_key_controller.rb
+++ app/controllers/fitbit_key_controller.rb
@@ -9,7 +9,7 @@
     })
     key.save!
 
-    quanto_plugin_id = OauthKey.where(provider: quanto, plugin: 'fitbit').pluck(:uid).last
+    quanto_plugin_id = OauthKey.where(provider: 'quanto', plugin: 'fitbit').pluck(:uid).last
     redirect_to "http://quanto.herokuapp.com/plugins/#{quanto_plugin_id}"
   end
 end
