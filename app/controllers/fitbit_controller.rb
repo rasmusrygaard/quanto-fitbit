e162e3fc50de2ef7700fe7a02ceff700ffb248a7
Reed Landrum <r.davis.landrum@gmail.com>
2014-02-04 15:54:52 -0800

    spacing is hard

--- app/controllers/fitbit_controller.rb
+++ app/controllers/fitbit_controller.rb
@@ -21,26 +21,26 @@ class FitbitController < ApplicationController
   end
   
   def poll
-	Mapping.find_each do |mapping|
-		fitbit_options = {
-		  consumer_key: ENV["FITBIT_KEY"],
-		  consumer_secret: ENV["FITBIT_SECRET"],
-		  token: mapping.fitbit_token,
-		  secret: mapping.fitbit_token_secret,
-		}
-		fitbit_client = Fitgem::Client.new(options);
-		range_options = { base_date: 1.days.ago, period: 'today' }
-		steps = client.data_by_time_range('/activities/log/steps', range_options)
-		sleep = client.data_by_time_range('/sleep/minutesAsleep', range_options)
+    Mapping.find_each do |mapping|
+	  fitbit_options = {
+        consumer_key: ENV["FITBIT_KEY"],
+		consumer_secret: ENV["FITBIT_SECRET"],
+		token: mapping.fitbit_token,
+		secret: mapping.fitbit_token_secret,
+      }
+      fitbit_client = Fitgem::Client.new(options);
+      range_options = { base_date: 1.days.ago, period: 'today' }
+      steps = client.data_by_time_range('/activities/log/steps', range_options)
+      sleep = client.data_by_time_range('/sleep/minutesAsleep', range_options)
 		
-		quanto_options = {
-		  consumer_key: ENV["QUANTO_KEY"],
-		  consumer_secret: ENV["QUANTO_SECRET"],
-		  access_token: mapping.quanto_access_token
-		}
-		quanto_client = Quanto::Client.new(options);
-		quanto_client.record_metric(steps[:"activities-log-steps"][0][:value], 'steps');
-		quanto_client.record_metric(sleep[:"sleep-minutesAsleep"][0][:value]/60.0, 'sleep'); #in hours
+      quanto_options = {
+        consumer_key: ENV["QUANTO_KEY"],
+        consumer_secret: ENV["QUANTO_SECRET"],
+        access_token: mapping.quanto_access_token
+      }
+      quanto_client = Quanto::Client.new(options);
+      quanto_client.record_metric(steps[:"activities-log-steps"][0][:value], 'steps');
+      quanto_client.record_metric(sleep[:"sleep-minutesAsleep"][0][:value]/60.0, 'sleep'); #in hours
 	end	
   end
 
