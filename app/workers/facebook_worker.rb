d9405798375dc3988934d196a2a3519880956d34
reedlandrum <rlandrum@stanford.edu>
2014-02-11 03:38:27 -0800

    facebook_worker logging

--- app/workers/facebook_worker.rb
+++ app/workers/facebook_worker.rb
@@ -4,14 +4,14 @@ class FacebookWorker
   def perform(mapping_id)
     mapping = Mapping.find(mapping_id)
  
-	graph = Koala::Facebook::API.new(mapping.api_key.token)
-	statuses = graph.get_connections("me", "statuses")
-	
-	statuses_count = 0
-	likes_count = 0
-	statuses[:data].each do |status|
+    graph = Koala::Facebook::API.new(mapping.api_key.token)
+    statuses = graph.get_connections("me", "statuses")
+    puts statuses.to_s	
+    statuses_count = 0
+    likes_count = 0
+    statuses[:data].each do |status|
       if(Date.parse(status[:updated_time]) === Date.today)
-	    statuses++
+        statuses++
         if(status[:likes].present?)
           likes += status[:likes].count
         end
@@ -22,7 +22,7 @@ class FacebookWorker
     quanto_client = Quanto::Client.new(ENV["QUANTO_FACEBOOK_KEY"], ENV["QUANTO_FACEBOOK_SECRET"],
                                 access_token: quanto_key.token)
     quanto_client.record_metric(statuses_count, :posts)
-	quanto_client.record_metric(likes_count, :likes)
+    quanto_client.record_metric(likes_count, :likes)
   end
 
   def self.record_all
