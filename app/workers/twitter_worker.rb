0dcbccd6401bd27a571922ad13eaae40caf0ce9c
reedlandrum <rlandrum@stanford.edu>
2014-02-15 09:31:33 -0800

    Twitter timeline parsing added

--- app/workers/twitter_worker.rb
+++ app/workers/twitter_worker.rb
@@ -10,13 +10,21 @@ class TwitterWorker
       config.access_token = mapping.api_key.token
       config.access_token_secret = mapping.api_key.token_secret
     end
-	
+
+    count = 0
+
     tweets = client.user_timeline(client.verify_credentials[:screen_name])
-	
+    
+    tweets.each do |tweet|
+      if(Date.parse(tweet.created_at.to_s) === Date.today)
+        count++
+      end
+    end
+
     quanto_key = mapping.quanto_key
     quanto_client = Quanto::Client.new(ENV["QUANTO_TWITTER_KEY"], ENV["QUANTO_TWITTER_SECRET"],
                                 access_token: quanto_key.token)
-    quanto_client.record_entry(tweets.count, :tweets)
+    quanto_client.record_entry(count, :tweets)
   end
 
   def self.record_all
