80865bebc4b38f66715de0eb782b90b66e56e9ec
reedlandrum <rlandrum@stanford.edu>
2014-02-25 16:33:06 -0800

    key controller for moves

---
+++ app/controllers/moves_key_controller.rb
@@ -0,0 +1,21 @@
+class MovesKeyController < ApplicationController
+
+  def create
+    auth = request.env["omniauth.auth"]
+    key = OauthKey.new({
+      provider: auth.provider,
+      uid: auth.uid,
+      token: auth.credentials.token,
+      plugin: 'moves',
+    })
+    key.save!
+
+    quanto_key = Mapping.create_mapping_for_key(key, session)
+
+    client = Quanto::Client.new(ENV["QUANTO_MOVES_KEY"], ENV["QUANTO_MOVES_SECRET"],
+                                access_token: quanto_key.token)
+    redirect_to client.plugin_url
+  end
+
+end
+
