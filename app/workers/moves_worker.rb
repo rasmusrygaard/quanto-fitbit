e9c70ad97c5c33621b71989eb93beac1624810f5
reedlandrum <rlandrum@stanford.edu>
2014-03-03 11:49:10 -0800

    parsing fixes

--- app/workers/moves_worker.rb
+++ app/workers/moves_worker.rb
@@ -10,21 +10,25 @@ class MovesWorker
     quanto_client = Quanto::Client.new(ENV["QUANTO_MOVES_KEY"], ENV["QUANTO_MOVES_SECRET"],
                                 access_token: quanto_key.token)
 
-    activities = client.daily_summary[:summary]
+    activities = client.daily_summary[0]["summary"]
 
     for activity in activities
-      if activity[:activity] == "walking"
+      if activity["activity"] == "walking"
         walking = activity
       end
-      if activity[:activity] == "cycling"
+      if activity["activity"] == "cycling"
         cycling = activity
       end
     end
 
-    quanto_client.record_entry(walking[:duration]/60.0, :"Time Walking");
-    quanto_client.record_entry(walking[:calories], :"Calories Burned Walking");
-    quanto_client.record_entry(cycling[:duration]/60.0, :"Time Cycling");
-    quanto_client.record_entry(cycling[:calories], :"Calories Burned Cycling");
+    if defined?(walking)
+      quanto_client.record_entry(walking["duration"]/60.0, :"Time Walking");
+      quanto_client.record_entry(walking["calories"], :"Calories Burned Walking");
+    end
+    if defined?(cycling)
+      quanto_client.record_entry(cycling["duration"]/60.0, :"Time Cycling");
+      quanto_client.record_entry(cycling["calories"], :"Calories Burned Cycling");
+    end
   end
 
   def self.record_all
