41b0b65d1ccaf1a2df4532d1b04b9e797b95cb21
reedlandrum <rlandrum@stanford.edu>
2014-03-04 19:04:14 -0800

    remove backlog-pulling code

--- app/workers/moves_worker.rb
+++ app/workers/moves_worker.rb
@@ -31,47 +31,8 @@ class MovesWorker
     end
   end
 
-
-  def self.pull_from_date(mapping_id, date)
-    mapping = Mapping.find(mapping_id)
- 
-    client = Moves::Client.new(mapping.api_key.token)
-	
-    quanto_key = mapping.quanto_key
-    quanto_client = Quanto::Client.new(ENV["QUANTO_MOVES_KEY"], ENV["QUANTO_MOVES_SECRET"],
-                                access_token: quanto_key.token)
-
-    activities = client.daily_summary(date.to_s)[0]["summary"]
-    
-    if activities.nil?
-      return
-    end
-
-    for activity in activities
-      if activity["activity"] == "walking"
-        walking = activity
-      end
-      if activity["activity"] == "cycling"
-        cycling = activity
-      end
-    end
-
-    if defined?(walking) and !walking.nil?
-      quanto_client.record_entry(walking["duration"]/60.0, :"Time Walking", { :date => date.to_s });
-      quanto_client.record_entry(walking["calories"], :"Calories Burned Walking", { :date => date.to_s });
-    end
-    if defined?(cycling) and !cycling.nil?
-      quanto_client.record_entry(cycling["duration"]/60.0, :"Time Cycling", { :date => date.to_s });
-      quanto_client.record_entry(cycling["calories"], :"Calories Burned Cycling", { :date => date.to_s });
-    end
-  end
-
   def self.record_all
     Mapping.moves.find_each { |mapping| MovesWorker.perform_async(mapping.id) }
-  end
-
-  def self.pull_backlog
-    Date.parse("2014-02-01").upto(Date.today) { |date| Mapping.moves.find_each { |mapping| MovesWorker.pull_from_date(mapping.id, date) } }
   end
 
 end
