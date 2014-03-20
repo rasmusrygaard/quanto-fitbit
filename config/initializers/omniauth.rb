0c1edf39ce9c0e5ef367e7c04089cb5acd3ed458
reedlandrum <rlandrum@stanford.edu>
2014-03-20 06:47:18 -0700

    Add user_statuses to fb's scope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :fitbit, ENV["FITBIT_KEY"], ENV["FITBIT_SECRET"]
   provider :lastfm, ENV["LASTFM_KEY"], ENV["LASTFM_SECRET"]
   provider :instagram, ENV["INSTAGRAM_KEY"], ENV["INSTAGRAM_SECRET"]
-  provider :facebook, ENV["FACEBOOK_KEY"], ENV["FACEBOOK_SECRET"]
+  provider :facebook, ENV["FACEBOOK_KEY"], ENV["FACEBOOK_SECRET"], { :scope => "user_status"}
   provider :moves, ENV["MOVES_KEY"], ENV["MOVES_SECRET"], { :scope => "activity location"}
   provider :twitter, ENV["TWITTER_KEY"], ENV["TWITTER_SECRET"]
 
