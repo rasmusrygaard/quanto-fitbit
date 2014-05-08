fc23cc745b295b29b1ca9a79409710c3559256a9
Rasmus Rygaard <rasmus@rasmusrygaard.com>
2014-05-08 15:43:41 -0700

    Fire job on received update

--- app/controllers/instagram_key_controller.rb
+++ app/controllers/instagram_key_controller.rb
@@ -11,7 +11,9 @@ class InstagramKeyController < ApplicationController
     key.save!
 
     client = Instagram.client(access_token: auth.credentials.token)
-    client.create_subscription(:user, 'http://quanto-plugins.herokuapp.com/instagram_subscription')
+    client.create_subscription(:user,
+                               'http://quanto-plugins.herokuapp.com/instagram_subscription',
+                               verify_token: ENV['INSTAGRAM_VERIFY_TOKEN'])
 
     quanto_key = Mapping.create_mapping_for_key(key, session)
 
--- app/controllers/instagram_subscription_controller.rb
+++ app/controllers/instagram_subscription_controller.rb
@@ -1,22 +1,30 @@
 class InstagramSubscriptionController < ApplicationController
 
-  def create
-    render text: "not authorized", status: 401 unless Instagram.validate_update(request.body, headers)
+  respond_to :json
 
-    Instagram.process_subscription(params[:body]) do |handler|
+  # Skip verification since POST comes from Instagram.
+  protect_from_forgery :except => :create
+
+  def create
+    # Can't verify request because heroku strips X header.
+    # if Instagram.validate_update(params['_json'].to_json, headers)
+
+    Instagram.process_subscription(params['_json'].to_json) do |handler|
 
       handler.on_user_changed do |user_id, data|
-        key = OauthKey.instagram.where(uid: user_id.to_s)
+        key = OauthKey.instagram.where(uid: user_id.to_s).last
         mapping = Mapping.where(api_key: key).first
         InstagramWorker.perform_async(mapping.id)
       end
 
     end
+
+    render json: "ok", status: 200
   end
 
   def index
-    valid_params = Instagram.client.meet_challenge(params)
-    render json: valid_params if valid_params
+    valid_params = Instagram.client.meet_challenge(params, ENV['INSTAGRAM_VERIFY_TOKEN'])
+    render json: (valid_params || 'error')
   end
 
 end
