b2b1c3b7d09838028cddab84596eb50bce76167a
Rasmus Rygaard <rasmus@rasmusrygaard.com>
2014-05-08 15:20:45 -0700

    Use Instagram client for challenge, set up worker for responses.

--- app/controllers/instagram_subscription_controller.rb
+++ app/controllers/instagram_subscription_controller.rb
@@ -1,15 +1,22 @@
 class InstagramSubscriptionController < ApplicationController
 
   def create
+    Instagram.process_subscription(params[:body]) do |handler|
 
+      handler.on_user_changed do |user_id, data|
+        key = OauthKey.instagram.where(uid: user_id.to_s)
+        user = User.by_instagram_id(user_id)
+        @client = Instagram.client(:access_token => _access_token_for_user(user))
+        latest_media = @client.user_recent_media[0]
+        user.media.create_with_hash(latest_media)
+      end
+
+    end
   end
 
   def index
-    if params['hub.mode'] && params['hub.mode'] == 'subscribe'
-      render json: params['hub.challenge'], status: 200
-    else
-      render json: "error", status: 400
-    end
+    valid_params = Instagram.client.meet_challenge(params)
+    render json: valid_params if valid_params
   end
 
 end
