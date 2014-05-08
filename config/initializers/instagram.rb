dbaf5746ea72f5049aba58da832dee2afd75e664
Rasmus Rygaard <rasmus@rasmusrygaard.com>
2014-05-08 14:37:14 -0700

    Fixed Instagram credentials.

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -2,5 +2,5 @@ require "instagram"
 
 Instagram.configure do |config|
   config.client_id = ENV["INSTAGRAM_KEY"]
-  config.access_token = ENV["INSTAGRAM_KEY"]
+  config.client_secret = ENV["INSTAGRAM_SECRET"]
 end
